cb351d201b36760f404efec921315935aa46460b
Chukwuma Okoroji <chuck@Chucks-Air.lan>
2022-09-01 01:19:19 -0400

    replaced express with nestjs

--- src/routes/example.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/example', function(req, res) {
-    res.send('This is the example page');
-});
-
-module.exports = router;
